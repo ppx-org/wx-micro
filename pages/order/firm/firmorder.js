5cfb43d8492cb9211b2c2253448830b71073b86a
dengxz <6882855@qq.com>
2018-01-23 23:04:15 +0800

    Signed-off-by: dengxz <6882855@qq.com>

--- pages/order/firm/firmorder.js
+++ pages/order/firm/firmorder.js
@@ -1,66 +1,28 @@
-// pages/order/firm/firmorder.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    prodList: [],
+    allBottomColor: "red",
+    waitBottomColor: "white"
+  },
+  onLoad: function () {
+    var r = { "swiperList": [], "actionStatus": "OK", "levelList": [{ "levelId": 1, "levelName": "店长推荐", "firstProdId": 1 }, { "levelId": 2, "levelName": "第二层", "firstProdId": 5 }], "errorCode": "0", "page": { "pageSize": 6, "pageNumber": 1, "totalRows": 0, "hasMore": false }, "prodList": [{ "pid": 1, "p": "9.80", "t": "test1我是化装品", "f": 0, "level": { "levelId": 1, "levelName": "第一层", "firstProdId": 1 } }, { "pid": 2, "p": "6.90", "t": "test2我是化装品", "f": 1, "level": null }, { "pid": 3, "p": "7.90", "t": "test3", "f": 1, "level": null }] };
+    this.setData({
+      prodList: r.prodList
+    })
+
+    //this.openFilter();
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  allOrder: function () {
+    this.setData({
+      allBottomColor: "red",
+      waitBottomColor: "white"
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  allOrder2: function () {
+    this.setData({
+      allBottomColor: "white",
+      waitBottomColor: "red"
+    });
   }
 })
