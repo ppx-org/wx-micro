09303fc067404ad42646be6aa546798829b19d75
dengxz <6882855@qq.com>
2018-01-24 21:48:37 +0800

    Signed-off-by: dengxz <6882855@qq.com>

--- pages/product/product.js
+++ pages/product/product.js
@@ -3,9 +3,32 @@ const app = getApp()
 
 Page({
   data: {
-    
+    skuBottom:-890
   },
   onLoad: function (option) {
     console.log(option.id)
+  },
+  favorite: function() {
+    wx.showToast({ title: "收藏成功", icon:"none"})
+  },
+  addToCart: function() {
+    this.setData({
+      skuBottom:0
+    });
+  },
+  buy: function() {
+    this.setData({
+      skuBottom: 0
+    });
+  },
+  closeSku:function() {
+    this.setData({
+      skuBottom: -890
+    });
+  },
+  gotoFirmOrder:function() {
+    wx.navigateTo({
+      url: '/pages/order/firm/firmorder',
+    })
   }
 })
