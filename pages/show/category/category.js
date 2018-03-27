319c68013c89c1855f51d5345640838c76285c33
dengxz <6882855@qq.com>
2018-03-27 22:08:38 +0800

    Signed-off-by: dengxz <6882855@qq.com>

--- pages/show/category/category.js
+++ pages/show/category/category.js
@@ -4,12 +4,22 @@ const app = getApp()
 Page({
   data: {
     list:[],
-    backUrl:"http://test-1253864162.file.myqcloud.com/cat.png"
+    backUrl:""
   },
   onLoad: function () {
     var thisPage = this;
     getApp().request("MQuery/listCategory", null, function (r) {
+      var len = r.arrayList.length;
+      for (var i = 0; i < len; i++) {
+        
+        var patchN = 4 - r.arrayList[i].children.length;
+        for (var j = 0; j < patchN; j++) {
+          r.arrayList[i].children.push({backColor:"#EEEEEE"});
+        }
+      }
+      console.log(r.arrayList);
       thisPage.setData({
+        backUrl: r.imgSrc,
         list: r.arrayList
       })
     })
