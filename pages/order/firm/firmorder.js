da7503d137d4a4451c960c7525d14b3544eee07b
dengxz <6882855@qq.com>
2018-04-04 21:43:53 +0800

    Signed-off-by: dengxz <6882855@qq.com>

--- pages/order/firm/firmorder.js
+++ pages/order/firm/firmorder.js
@@ -4,40 +4,32 @@
 Page({
   data: {
     IMG_URL: getApp().globalData.IMG_URL,
+    store: {},
 	  skuIndexList:[],
 	  totalNum:0,
-	  totalPrice:0,
-	  
-	// 改成skuList
-    prodList: [],
-    allBottomColor: "red",
-    waitBottomColor: "white"
+	  totalPrice:0
   },
   onLoad: function (options) {
-    var r = { "swiperList": [], "actionStatus": "OK", "levelList": [{ "levelId": 1, "levelName": "店长推荐", "firstProdId": 1 }, { "levelId": 2, "levelName": "第二层", "firstProdId": 5 }], "errorCode": "0", "page": { "pageSize": 6, "pageNumber": 1, "totalRows": 0, "hasMore": false }, "prodList": [{ "pid": 1, "p": "9.80", "t": "test1我是化装品", "f": 0, "level": { "levelId": 1, "levelName": "第一层", "firstProdId": 1 } }, { "pid": 2, "p": "6.90", "t": "test2我是化装品", "f": 1, "level": null }, { "pid": 3, "p": "7.90", "t": "test3", "f": 1, "level": null }] };
-    this.setData({
-      prodList:r.prodList
-    })
-
-	
-	var skuId = [];
-  options.skuIds.split(",").forEach(function(o) {skuId.push(parseInt(o))});
-  var num = [];
-  options.nums.split(",").forEach(function (o) { num.push(parseInt(o)) });
-  
-  console.log(skuId, num);
-	
-	var para = {skuId:skuId, num:num}
-	var thisPage = this;
-    getApp().request("MOrder/countPrice", para, function (r) {
+    var skuId = [];
+    options.skuIds.split(",").forEach(function(o) {skuId.push(parseInt(o))});
+    var num = [];
+    options.nums.split(",").forEach(function (o) { num.push(parseInt(o)) });
+    
+    var para = {skuId:skuId, num:num}
+    var thisPage = this;
+    getApp().request("MOrder/confirmOrder", para, function (r) {
       thisPage.setData({
-        skuIndexList:r.skuIndexList,
-		    totalNum:r.totalNum,
-		    totalPrice:r.totalPrice
+        store: r.store,
+        skuIndexList: r.skuList,
+        totalNum:r.totalNum,
+        totalPrice:r.totalPrice
       })
     })
   },
-
+  map: function () {
+    var s = this.data.store;
+    wx.openLocation({ latitude: parseFloat(s.lat), longitude: parseFloat(s.lng), scale: 18, name: s.name, address: s.addr });
+  },
   submitOrder: function () {
     
   }
