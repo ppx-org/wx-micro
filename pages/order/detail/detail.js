a9b899b1537ef6de880bb98f6bc73104ddef81c6
dengxz <6882855@qq.com>
2018-04-05 20:27:13 +0800

    Signed-off-by: dengxz <6882855@qq.com>

--- pages/order/detail/detail.js
+++ pages/order/detail/detail.js
@@ -1,5 +1,29 @@
+
+
+
 Page({
   data: {
-    
+    IMG_URL: getApp().globalData.IMG_URL,
+    store:{},
+    order:{},
+    statusList:[]
+  },
+  onLoad: function (option) {
+    var orderId = 3;//option.orderId;
+    var thisPage = this;
+
+    getApp().request("MOrder/getDetail?orderId=" + orderId, null, function (r) {
+      //  orderNum
+      r.order.orderNum = 0;
+      r.order.listItem.forEach(function (item) {
+        r.order.orderNum += item.itemNum;
+      })
+
+      thisPage.setData({
+        store: r.store,
+        order: r.order,
+        statusList: r.statusList
+      })
+    })
   }
 })
